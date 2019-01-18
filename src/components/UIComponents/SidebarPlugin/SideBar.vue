da1aefdda2d1f4a1b2e53f418287cfc35ed7616c
Andy Davis <github@andy-davis.co.uk>
2019-01-18 23:53:37 +0000

    Dashboard template almost working, needs sidebar and topnav fix

---
+++ src/components/UIComponents/SidebarPlugin/SideBar.vue
@@ -0,0 +1,116 @@
+<template>
+  <div :class="sidebarClasses"
+       :data-background-color="backgroundColor"
+       :data-active-color="activeColor">
+    <div class="logo">
+      <router-link to="/" tag="div" class="simple-text logo-mini">
+          <div class="logo-img">
+              <a>
+                <img :src="logo" alt="" >
+              </a>
+          </div>
+      </router-link>
+    </div>
+    <div class="sidebar-wrapper" ref="sidebarScrollArea">
+      <slot>
+
+      </slot>
+      <ul :class="navClasses">
+        <slot name="links">
+          <sidebar-item v-for="(link, index) in sidebarLinks"
+                        :key="link.name + index"
+                        :link="link">
+
+            <sidebar-item v-for="(subLink, index) in link.children"
+                          :key="subLink.name + index"
+                          :link="subLink">
+            </sidebar-item>
+          </sidebar-item>
+        </slot>
+
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: 'VoX Merchant Wallet'
+      },
+      logo: {
+          type: String,
+          default: '../../../../static/img/voxwallet-logo.png'
+        },
+      type: {
+        type: String,
+        default: 'sidebar',
+        validator: (value) => {
+          let acceptedValues = ['sidebar', 'navbar']
+          return acceptedValues.indexOf(value) !== -1
+        }
+      },
+      backgroundColor: {
+        type: String,
+        default: 'black',
+        validator: (value) => {
+          let acceptedValues = ['white', 'brown', 'black']
+          return acceptedValues.indexOf(value) !== -1
+        }
+      },
+      activeColor: {
+        type: String,
+        default: 'success',
+        validator: (value) => {
+          let acceptedValues = ['primary', 'info', 'success', 'warning', 'danger']
+          return acceptedValues.indexOf(value) !== -1
+        }
+      },
+      sidebarLinks: {
+        type: Array,
+        default: () => []
+      }
+    },
+    computed: {
+      sidebarClasses () {
+        if (this.type === 'sidebar') {
+          return 'sidebar'
+        } else {
+          return 'collapse navbar-collapse off-canvas-sidebar'
+        }
+      },
+      navClasses () {
+        if (this.type === 'sidebar') {
+          return 'nav'
+        } else {
+          return 'nav navbar-nav'
+        }
+      }
+    },
+    methods: {
+      async initScrollBarAsync () {
+        await import('../../../../node_modules/perfect-scrollbar/css/perfect-scrollbar.css')
+        const PerfectScroll = await import('perfect-scrollbar')
+        PerfectScroll.initialize(this.$refs.sidebarScrollArea)
+      }
+    },
+    mounted () {
+      this.initScrollBarAsync()
+    },
+    beforeDestroy () {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.showSidebar = false
+      }
+    }
+  }
+
+</script>
+<style>
+  @media (min-width: 992px) {
+    .navbar-search-form-mobile,
+    .nav-mobile-menu{
+      display: none;
+    }
+  }
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,199 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Dashboard from './views/Dashboard.vue';
+import DashboardLayout from './components/Dashboard/Layout/DashboardLayout.vue'
+import Overview from './components/Dashboard/Views/Dashboard/Overview.vue'
+import Stats from './components/Dashboard/Views/Dashboard/Stats.vue'
+
+// Pages
+import User from './components/Dashboard/Views/Pages/UserProfile.vue'
+import TimeLine from './components/Dashboard/Views/Pages/TimeLinePage.vue'
+import Login from './components/Dashboard/Views/Pages/Login.vue'
+import Register from './components/Dashboard/Views/Pages/Register.vue'
+import Lock from './components/Dashboard/Views/Pages/Lock.vue'
+
+// Components pages
+import Buttons from './components/Dashboard/Views/Components/Buttons.vue'
+import GridSystem from './components/Dashboard/Views/Components/GridSystem.vue'
+import Panels from './components/Dashboard/Views/Components/Panels.vue'
+import SweetAlert from './components/Dashboard/Views/Components/SweetAlert.vue'
+import Notifications from './components/Dashboard/Views/Components/Notifications.vue'
+import Icons from './components/Dashboard/Views/Components/Icons.vue'
+import Typography from './components/Dashboard/Views/Components/Typography.vue'
+
+// Forms pages
+import RegularForms from './components/Dashboard/Views/Forms/RegularForms.vue'
+import ExtendedForms from './components/Dashboard/Views/Forms/ExtendedForms.vue'
+import ValidationForms from './components/Dashboard/Views/Forms/ValidationForms.vue'
+import Wizard from './components/Dashboard/Views/Forms/Wizard.vue'
+
+// TableList pages
+import RegularTables from './components/Dashboard/Views/Tables/RegularTables.vue'
+import ExtendedTables from './components/Dashboard/Views/Tables/ExtendedTables.vue'
+import PaginatedTables from './components/Dashboard/Views/Tables/PaginatedTables.vue'
+// Maps pages
+import GoogleMaps from './components/Dashboard/Views/Maps/GoogleMaps.vue'
+import FullScreenMap from './components/Dashboard/Views/Maps/FullScreenMap.vue'
+import VectorMaps from './components/Dashboard/Views/Maps/VectorMapsPage.vue'
+
+// Calendar
+import Calendar from './components/Dashboard/Views/Calendar/CalendarRoute.vue'
+// Charts
+import Charts from './components/Dashboard/Views/Charts.vue'
+
+let componentsMenu = {
+  path: '/components',
+  component: DashboardLayout,
+  redirect: '/components/buttons',
+  children: [
+    {
+      path: 'buttons',
+      name: 'Buttons',
+      component: Buttons
+    },
+    {
+      path: 'grid-system',
+      name: 'Grid System',
+      component: GridSystem
+    },
+    {
+      path: 'panels',
+      name: 'Panels',
+      component: Panels
+    },
+    {
+      path: 'sweet-alert',
+      name: 'Sweet Alert',
+      component: SweetAlert
+    },
+    {
+      path: 'notifications',
+      name: 'Notifications',
+      component: Notifications
+    },
+    {
+      path: 'icons',
+      name: 'Icons',
+      component: Icons
+    },
+    {
+      path: 'typography',
+      name: 'Typography',
+      component: Typography
+    }
+
+  ]
+}
+let formsMenu = {
+  path: '/forms',
+  component: DashboardLayout,
+  redirect: '/forms/regular',
+  children: [
+    {
+      path: 'regular',
+      name: 'Regular Forms',
+      component: RegularForms
+    },
+    {
+      path: 'extended',
+      name: 'Extended Forms',
+      component: ExtendedForms
+    },
+    {
+      path: 'validation',
+      name: 'Validation Forms',
+      component: ValidationForms
+    },
+    {
+      path: 'wizard',
+      name: 'Wizard',
+      component: Wizard
+    }
+  ]
+}
+
+let tablesMenu = {
+  path: '/table-list',
+  component: DashboardLayout,
+  redirect: '/table-list/regular',
+  children: [
+    {
+      path: 'regular',
+      name: 'Regular Tables',
+      component: RegularTables
+    },
+    {
+      path: 'extended',
+      name: 'Extended Tables',
+      component: ExtendedTables
+    },
+    {
+      path: 'paginated',
+      name: 'Paginated Tables',
+      component: PaginatedTables
+    }]
+}
+
+let mapsMenu = {
+  path: '/maps',
+  component: DashboardLayout,
+  redirect: '/maps/google',
+  children: [
+    {
+      path: 'google',
+      name: 'Google Maps',
+      component: GoogleMaps
+    },
+    {
+      path: 'full-screen',
+      name: 'Full Screen Map',
+      component: FullScreenMap
+    },
+    {
+      path: 'vector-map',
+      name: 'Vector Map',
+      component: VectorMaps
+    }
+  ]
+}
+
+let pagesMenu = {
+  path: '/pages',
+  component: DashboardLayout,
+  redirect: '/pages/user',
+  children: [
+    {
+      path: 'user',
+      name: 'User Page',
+      component: User
+    },
+    {
+      path: 'timeline',
+      name: 'Timeline Page',
+      component: TimeLine
+    }
+  ]
+}
+
+let loginPage = {
+  path: '/login',
+  name: 'Login',
+  component: Login
+}
+
+let registerPage = {
+  path: '/register',
+  name: 'Register',
+  component: Register
+}
+
+let lockPage = {
+  path: '/lock',
+  name: 'Lock',
+  component: Lock
+}
+
 
 Vue.use(Router);
 
@@ -20,6 +213,31 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+    componentsMenu,
+    formsMenu,
+    tablesMenu,
+    mapsMenu,
+    pagesMenu,
+    loginPage,
+    registerPage,
+    lockPage,
+    {
+      path: '/dashboard',
+      component: DashboardLayout,
+      redirect: '/dashboard/overview',
+      children: [
+        {
+          path: 'overview',
+          name: 'Overview',
+          component: Overview
+        },
+        {
+          path: 'stats',
+          name: 'Stats',
+          component: Stats
+        }
+      ]
     },
   ],
 });
