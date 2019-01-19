3fbf99c47578f7837419c2fea98b2b6f037b6d98
shaneaben <164257@supinfo.com>
2019-01-19 16:00:18 +0000

    add TypeScript to babel preset and remove index.js file from SidebarPlugin

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: [
+    '@babel/typescript',
     '@vue/app'
-  ]
+  ],
+	"plugins": [
+			"@babel/proposal-class-properties",
+			"@babel/proposal-object-rest-spread"
+	]
 }
