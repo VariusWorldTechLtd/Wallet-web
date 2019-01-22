0f368d7e162b2800fd9f260ede0df076797239d4
Andy Davis <me@andy-davis.co.uk>
2019-01-22 00:02:43 +0000

    Footer cleanup

--- src/components/Dashboard/Layout/ContentFooter.vue
+++ src/components/Dashboard/Layout/ContentFooter.vue
@@ -1,30 +1,10 @@
 <template>
   <footer class="footer">
     <div class="container-fluid">
-      <nav class="pull-left">
-        <ul>
-          <li>
-            <a href="http://www.creative-tim.com">
-              Creative Tim
-            </a>
-          </li>
-          <li>
-            <a href="http://blog.creative-tim.com">
-              Blog
-            </a>
-          </li>
-          <li>
-            <a href="http://www.creative-tim.com/license">
-              Licenses
-            </a>
-          </li>
-        </ul>
-      </nav>
       <div class="copyright pull-right">
         &copy; Coded with
         <i class="fa fa-heart heart"></i> by
-        <a href="https://github.com/cristijora" target="_blank">Cristi Jora</a>.
-        <span>Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">Creative Tim</a>.</span>
+        <a href="https://variusworldtech.com" target="_blank">Varius World Tech Ltd</a>.
       </div>
     </div>
   </footer>
