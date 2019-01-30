f13758eee64efc7e22b14b8699dd4df6d08f3fb0
Andy Davis <github@andy-davis.co.uk>
2019-01-30 23:44:51 +0000

    local copy of token json, nginx ssl and docker compose updates, err hndl

--- src/vsso_faucet/index.js
+++ src/vsso_faucet/index.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3')
-const vssoToken = require('../contracts/truffle/build/contracts/VssoToken.json');
+const vssoToken = require('./VssoToken.json');
 const vssoTokenAddress = '0x92fCc43e8FEda3CF74BF2A1A70fC456008Bd5b3C';
 const hotWalletMnemonic = 'plunge journey march test patch zebra jeans victory any chest remember antique';
 const voxnetRpc = 'https://voxwallet.vwtbet.com:8545'
@@ -10,14 +10,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
 app.get('/', async (req, res) => {
+  try {
     let transactionHash = '';
 
     const address = req.query.address;
 
-    if (!address) res.send('Usage: GET to ' + req.headers.host + '?address=ethereum_address_here')
-    const web3 = new Web3(new HDWalletProvider(hotWalletMnemonic, voxnetRpc ));
+    if (!address) {
+        res.send('Usage: GET to ' + req.headers.host + '?address=ethereum_address_here')
+        return
+    }
+
+    const web3 = new Web3(new HDWalletProvider(hotWalletMnemonic, voxnetRpc));
+
     let accounts = await web3.eth.getAccounts();
 
     let contract = new web3.eth.Contract(vssoToken.abi, vssoTokenAddress);
@@ -26,10 +31,16 @@ app.get('/', async (req, res) => {
         .on('receipt', receipt => {
             transactionHash = receipt.transactionHash;
         })
+        .on('error', error => {
+            res.send(error)
+        })
         //.then(console.log)
         .catch(console.error);
 
     res.send('Successfully sent ' + numberOfTokensToIssue + ' VSSO token to ' + address + 'in transactionHash: ' + transactionHash);
+  } catch (error) {
+      res.send(error);
+  }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
