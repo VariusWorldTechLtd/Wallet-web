96fd1218734a40e40fbed231a870b994831d990f
Andy Davis <me@andy-davis.co.uk>
2019-02-13 08:13:37 +0000

    part way to fixing the login test

---
+++ tests/e2e/specs/deposit.js
@@ -0,0 +1,18 @@
+// import './common_steps/login'
+
+// let browser;
+
+// module.exports = {
+//   '\n\
+//    In order to view account on web \n\
+//    As a Consumer \n\
+//    I want to login via QR code like whatsapp web'
+//   : theBrowser => {
+//     browser = theBrowser
+
+//       GIVEN_I_have_logged_in();
+      
+      
+//     browser.end();
+//   }
+// }
--- tests/e2e/specs/login_logout_test.js
+++ tests/e2e/specs/login_logout_test.js
@@ -2,10 +2,12 @@
 // http://nightwatchjs.org/guide#usage
 
 const voxnetRpcUrl = 'https://voxwallet2.vwtbet.com:8545';
+const voxnetWs = 'wss://voxwallet2.vwtbet.com:8546';
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3')
 const vssoToken = require('../../../src/contracts/truffle/build/contracts/VssoToken.json');
+const loginSession = require('../../../src/contracts/truffle/build/contracts/LoginSession.json');
 const vssoTokenAddress = '0x92fCc43e8FEda3CF74BF2A1A70fC456008Bd5b3C';
 
 const syncAccountsButton = 'button[name=sync]';
@@ -33,10 +35,9 @@ module.exports = {
 
       GIVEN_I_have_a_clean_profile();
       and_I_have_requested_a_login_QR();
-      WHEN_I_login_by_sending_VSSO_tokens_to_the_qr_address(); // simulating mobile action
-      and_I_save_user_data_to_the_login_session_contract(); // simulating mobile
+      WHEN_I_login_by_sending_VSSO_tokens_to_the_qr_address_and_data('firstname', 'lastname', 44, 'gender'); // simulating mobile action
       THEN_I_am_logged_in_to_dashboard();
-      and_my_details_are_present('firstname', 'lastname', 'age', 'gender');
+      and_my_details_are_present('firstname', 'lastname', 44, 'gender');
       and_I_can_logout();
       
     browser.end();
@@ -58,17 +59,18 @@ function and_I_have_requested_a_login_QR() {
     .waitForElementVisible(qrCodeButton, 10000);
 }
 
-function WHEN_I_login_by_sending_VSSO_tokens_to_the_qr_address() {
+function WHEN_I_login_by_sending_VSSO_tokens_to_the_qr_address_and_data(firstname, lastname, age, gender) {
   browser
     .getAttribute(qrCodeButton, 'value', async function (result) {
       loginSessionContractAddress = result.value;
-      await sendTokensTo(loginSessionContractAddress);
+      let mnemonic = 'plunge journey march test patch zebra jeans victory any chest remember antique';
+      const web3 = new Web3();
+      web3.setProvider(new HDWalletProvider(mnemonic, voxnetRpcUrl));
+
+      await sendTokensTo(loginSessionContractAddress, web3);
+      await saveUserData(web3, firstname, lastname, age, gender);
     });
   return loginSessionContractAddress;
-}
-
-function and_I_save_user_data_to_the_login_session_contract() {
-  //await sendTokensTo(loginSessionContractAddress);
 }
 
 function THEN_I_am_logged_in_to_dashboard() {
@@ -79,6 +81,7 @@ function THEN_I_am_logged_in_to_dashboard() {
 
 function and_my_details_are_present(firstname, lastname, age, gender) {
   browser
+    .waitForElementVisible(firstnameSpan, 20000)
     .assert.containsText(firstnameSpan, firstname)
     .assert.containsText(lastnameSpan, lastname)
     .assert.containsText(ageSpan, age)
@@ -91,10 +94,7 @@ function and_I_can_logout() {
     .waitForElementVisible('.hello', 5000);
 }
 
-async function sendTokensTo(toAddress) {
-  let mnemonic = 'plunge journey march test patch zebra jeans victory any chest remember antique';
-  const web3 = new Web3();
-  web3.setProvider(new HDWalletProvider(mnemonic, voxnetRpcUrl));
+async function sendTokensTo(toAddress, web3) {
   let accounts = await web3.eth.getAccounts();
   console.log('sending from account0', accounts[0], 'to', toAddress);
   let contract = new web3.eth.Contract(vssoToken.abi, vssoTokenAddress);
@@ -107,4 +107,42 @@ function clearLocalStorage() {
     window.localStorage.clear();
     return true;
   };
+}
+
+async function onSaveSessionEvent(action) {
+  const web3ws = new Web3(new Web3.providers.WebsocketProvider(voxnetWs));
+  let loginSessionContractWs = new web3ws.eth.Contract(loginSession.abi, loginSessionContractAddress);
+    
+  const options = {
+    filter: {
+      // _from:  process.env.WALLET_FROM,
+      // _to: contractAddress,
+      // _value: process.env.AMOUNT
+    },
+    fromBlock: 'latest'
+  };
+  loginSessionContractWs.once('SaveSessionEvent', options, async (error, event) => {
+    if (error) {
+      console.log(error);
+      successCallback(false);
+    }
+    await action();
+  });
+}
+
+async function saveUserData(web3, firstname, lastname, age, gender) {
+  let accounts = await web3.eth.getAccounts();
+
+  let loginSessionContract = new web3.eth.Contract(loginSession.abi, loginSessionContractAddress);
+
+  await loginSessionContract.methods.SaveData(firstname, lastname, age.toString(), gender)
+    .send({ from: accounts[0], gasPrice: 0, gas: 990000 })
+    // .on('receipt', receipt => {
+    //   transactionHash = receipt.transactionHash;
+    // })
+    .on('error', error => {
+      console.log('error', error);
+    })
+    .then(console.log('sent user data to: ' + loginSessionContractAddress))
+    .catch(console.error);
 }
